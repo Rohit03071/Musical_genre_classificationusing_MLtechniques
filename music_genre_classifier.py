21c4c72d4c60079642d55612db6829eadefb6850
Mr Rohit Aglawe <92461824+Rohit03071@users.noreply.github.com>
2024-09-30 03:03:52 +0530

    Update music_genre_classifier.py

--- music_genre_classifier.py
+++ music_genre_classifier.py
@@ -3,10 +3,11 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import keras
+import matplotlib.pyplot as plt
 
 
 #load the data from dataset
-DATASET_PATH = "data.json"
+DATASET_PATH = "data_10.json"
 
 def load_dataset(dataset_path):
     with open(dataset_path, "r") as fp:
@@ -17,6 +18,28 @@ def load_dataset(dataset_path):
         targets = np.array(data["labels"])
 
         return inputs, targets
+    
+
+def plot_history(history):
+    fig, axs = plt.subplots(2, 1)
+
+    #create the accuracy subplots
+
+    axs[0].plot(history.history["accuracy"], label = "train_accuracy")
+    axs[0].plot(history.history["val_accuracy"], label = "test_accuracy")
+    axs[0].set_ylabel("Accuracy")
+    axs[0].legend(loc="lower right")
+    axs[0].set_title("Accuracy eval")
+
+    axs[1].plot(history.history["loss"], label = "train_error")
+    axs[1].plot(history.history["val_loss"], label = "test_error")
+    axs[1].set_xlabel("Epoch")
+    axs[1].set_ylabel("Error")
+    axs[1].legend(loc="upper right")
+    axs[1].set_title("Error eval")
+
+    plt.show()
+
 
 if __name__ == "__main__":
     inputs, targets = load_dataset(DATASET_PATH)
@@ -30,37 +53,35 @@ if __name__ == "__main__":
 
     model = keras.Sequential([
 
-        keras.layers.Flatten(input_shape=(input.shape[1], input.shape[2])),
+        keras.layers.Flatten(input_shape=(inputs.shape[1], inputs.shape[2])),
 
         # 1st hidde layer
 
-        keras.layers.Dense(512, activation = "relu"),
+        keras.layers.Dense(512, activation = "relu", kernel_regularizer=keras.regularizers.l1(0.0001)),
 
-        keras.layers.Dense(256, activation = "relu"),
+        keras.layers.Dense(256, activation = "relu", kernel_regularizer=keras.regularizers.l1(0.0001)),
 
-        keras.layers.Dense(32, activation = "relu"),
+        keras.layers.Dense(32, activation = "relu", kernel_regularizer=keras.regularizers.l1(0.0001)),
+        keras.layers.Dropout(0.3),
 
-        keras.layers.Dense(10, activations="softmax")
+        keras.layers.Dense(10, activation="softmax")
 
     ])
 
     #compile the network
 
 
-    optimizer = keras.optimizers.Adam(learning_rate=0.001)
+    optimizer = keras.optimizers.Adam(learning_rate=0.0001)
 
-    model.compile(optimizer = optimizer, loss= "sparse_categorical_crossentropy", metrics=["accuracy"])
+    model.compile(optimizer = optimizer, 
+                  loss= "sparse_categorical_crossentropy", metrics=["accuracy"])
 
     model.summary()
 
+
     #tran the network
 
-    model.fit(inputs_train, targets_train, validation_data=(inputs_test, targets_test),
+    history = model.fit(inputs_train, targets_train, validation_data=(inputs_test, targets_test),
                                                             epochs = 50, 
                                                             batch_size=32)
-    
-
-
-
-
-
+    plot_history(history)
